e1a707870835eb4620f572898de20b223afed84d
felix <61442272+felix-prabowo@users.noreply.github.com>
2024-05-25 10:57:31 +0700

    Create and print the slot machine spin

--- slot_machine.py
+++ slot_machine.py
@@ -14,6 +14,38 @@ symbol_count = {
     "D": 8
 }
 
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+    
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]        # this will make a copy of the all_symbols list
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+    
+    return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+        
+        print()
+
+
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
@@ -43,6 +75,7 @@ def get_number_of_lines():
     
     return lines
 
+
 def get_bet():
     while True:
         amount = input("What would you like to bet on each line? $")
@@ -56,6 +89,7 @@ def get_bet():
             print("Please enter a number.")    
     
     return amount
+
 
 def main():
     balance = deposit()
@@ -71,6 +105,9 @@ def main():
             break
     
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 
 main()
